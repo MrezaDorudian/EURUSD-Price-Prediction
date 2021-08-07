ce09b21b027dd94a206f22c70073f663b1dcae80
Mohammad Reza <mr.dorudian@gmail.com>
2021-08-07 19:44:23 +0430

    class accuracy added

--- Evaluate.py
+++ Evaluate.py
@@ -1,6 +1,7 @@
 import matplotlib.pylab as plt
 import numpy as np
 
+
 class Evaluate:
     def __init__(self, model, history, changing_percentage, x_train, y_train, x_test, y_test, total_data, time_step):
         self.model = model
@@ -16,6 +17,8 @@ class Evaluate:
         self.train_accuracy = None
         self.test_loss = None
         self.test_accuracy = None
+        self.train_class_accuracy = None
+        self.test_class_accuracy = None
         pass
 
     def calculate_changing(self, class_number):
@@ -31,6 +34,45 @@ class Evaluate:
         plt.legend()
         plt.show()
 
+    def calculate_accuracy_for_each_class(self, x, y):
+        predicted = self.model.predict(x)
+        predicted_class = []
+        actual_class = y
+
+        for p in predicted:
+            predicted_class.append(np.argmax(p))
+        accuracy = [0, 0, 0, 0, 0]
+        counts = [list(actual_class).count(i) for i in range(5)]
+
+        for act, pred in zip(actual_class, predicted_class):
+            if act == pred:
+                accuracy[act] += 1
+        final_accuracy = [i / j for i, j in zip(accuracy, counts)]
+        return final_accuracy
+
+    def show_class_accuracy(self):
+        labels = ['Class 0', 'Class 1', 'Class 2', 'Class 3', 'Class 4']
+        train = self.train_class_accuracy
+        test = self.test_class_accuracy
+
+        x = np.arange(len(labels))
+        width = 0.35  # the width of the bars
+
+        fig, ax = plt.subplots()
+        rects1 = ax.bar(x - width / 2, train, width, label='Training set')
+        rects2 = ax.bar(x + width / 2, test, width, label='Testing set')
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel('Accuracy(%)')
+        ax.set_title('Accuracy by Training and Testing set')
+        ax.set_xticks(x)
+        ax.set_xticklabels(labels)
+        ax.legend()
+
+        fig.tight_layout()
+
+        plt.show()
+
     def prediction(self):
         predicted = np.argmax(self.model.predict(self.x_train[0:1]))
         actual = self.y_train[0]
@@ -39,9 +81,21 @@ class Evaluate:
         print(f'Price in the next 30 mins will change between {round(starting, 5)}% to {round(ending, 5)}% from now.')
         change = (((self.total_data.iloc[61]['Close'] / self.total_data.iloc[31]['Close']) - 1) * 100)
         print(f'actual change is: {round(change, 5)}')
+
+
     def run(self):
-        self.train_loss, self.train_accuracy = self.model.evaluate(self.x_train, self.y_train)
-        self.test_loss, self.test_accuracy = self.model.evaluate(self.x_test, self.y_test)
-        self.prediction()
+        self.train_loss, self.train_accuracy = self.model.evaluate(self.x_train, self.y_train, verbose=0)
+        self.test_loss, self.test_accuracy = self.model.evaluate(self.x_test, self.y_test, verbose=0)
         self.show_accuracy()
+        self.train_class_accuracy = self.calculate_accuracy_for_each_class(self.x_train, self.y_train)
+        self.test_class_accuracy = self.calculate_accuracy_for_each_class(self.x_test, self.y_test)
+        self.show_class_accuracy()
+        additional_info = f"""
+        class 0: between {self.changing_percentage[0]} to {self.changing_percentage[1]}.
+        class 1: between {self.changing_percentage[1]} to {self.changing_percentage[2]}.
+        class 2: between {self.changing_percentage[2]} to {self.changing_percentage[3]}.
+        class 3: between {self.changing_percentage[3]} to {self.changing_percentage[4]}.
+        class 4: between {self.changing_percentage[4]} to {self.changing_percentage[5]}.
+        """
+        print(additional_info)
 
