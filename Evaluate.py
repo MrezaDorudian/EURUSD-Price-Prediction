0e91eed8fd1975867c4b8870d2793be50ee37588
Mohammad Reza <mr.dorudian@gmail.com>
2021-08-09 20:27:09 +0430

    Fixed prediction function

--- Evaluate.py
+++ Evaluate.py
@@ -3,17 +3,10 @@ import numpy as np
 
 
 class Evaluate:
-    def __init__(self, model, history, changing_percentage, x_train, y_train, x_test, y_test, total_data, learning_ratio, time_step):
+    def __init__(self, model, history, changing_percentage):
         self.model = model
         self.history = history
         self.changing_percentage = changing_percentage
-        self.x_train = x_train
-        self.y_train = y_train
-        self.x_test = x_test
-        self.y_test = y_test
-        self.total_data = total_data
-        self.learning_ratio = learning_ratio
-        self.time_step = time_step
         self.train_loss = None
         self.train_accuracy = None
         self.test_loss = None
@@ -43,6 +36,9 @@ class Evaluate:
         for act, pred in zip(actual_class, predicted_class):
             if act == pred:
                 accuracy[act] += 1
+        for i in range(len(counts)):
+            if counts[i] == 0:
+                counts[i] += 1
         final_accuracy = [i / j for i, j in zip(accuracy, counts)]
         return final_accuracy
 
@@ -63,47 +59,64 @@ class Evaluate:
         fig.tight_layout()
         plt.show()
 
-    def show_info(self):
+    def show_changing_percentage(self):
         print(f'class 1: between {round(self.changing_percentage[0], 5)}% to {round(self.changing_percentage[1], 5)}%.')
         print(f'class 2: between {round(self.changing_percentage[1], 5)}% to {round(self.changing_percentage[2], 5)}%.')
         print(f'class 3: between {round(self.changing_percentage[2], 5)}% to {round(self.changing_percentage[3], 5)}%.')
         print(f'class 4: between {round(self.changing_percentage[3], 5)}% to {round(self.changing_percentage[4], 5)}%.')
         print(f'class 5: between {round(self.changing_percentage[4], 5)}% to {round(self.changing_percentage[5], 5)}%.')
+
+    def show_info(self, maximum_upcoming_min, predicted_label, actual_change, actual_cass, plot_data, peak_class):
+        print(f'Peak will probably occur in {maximum_upcoming_min} minutes from now, and will change between {round(self.changing_percentage[peak_class], 5)}% and {round(self.changing_percentage[peak_class + 1], 5)}%.')
+        print(f'Price will probably change {round(self.changing_percentage[predicted_label], 5)}% to {round(self.changing_percentage[predicted_label + 1], 5)}% in 30 minutes from now.')
+        print(f'Actual change is: {actual_change}')
+        print(f'Predicted class is:{predicted_label}, actual class is: {actual_cass}')
+        print()
+        plt.plot(plot_data)
+        plt.show()
         pass
 
-    def find_peak(self, data_index):
-        all_period = self.x_test[data_index - 30 + 1:data_index + 1]
+    def predict(self, dataset, x, y, data_index):
+        dataset = dataset[30:]
+        past_time = dataset.iloc[data_index - 30 + 1]['Date']
+        past_price = dataset.iloc[data_index - 30 + 1]['Close']
+
+        current_time = dataset.iloc[data_index]['Date']
+        current_price = dataset.iloc[data_index]['Close']
+
+        future_time = dataset.iloc[data_index + 30]['Date']
+        future_price = dataset.iloc[data_index + 30]['Close']
+
+        all_period = x[data_index - 30 + 1:data_index + 1]
         prediction = self.model.predict(all_period)
         predicted_labels = []
         for candle in prediction:
             predicted_labels.append(np.argmax(candle))
         peak_class = max(predicted_labels)
-        valley_class = min(predicted_labels)
         max_output = []
-        min_output = []
         for i in range(len(predicted_labels)):
             if predicted_labels[i] == peak_class:
-                max_output.append(i + 1)
-            if predicted_labels[i] == valley_class:
-                min_output.append(i + 1)
-        print(f'Peak will probably occur in {max_output[len(max_output) // 2]} minutes from now, and will change between {round(self.changing_percentage[peak_class], 5)}% and {round(self.changing_percentage[peak_class + 1], 5)}%.')
-        print(max_output)
-        print(f'Valley will probably occur in {min_output[len(min_output) // 2]} minutes from now, and will change between {round(self.changing_percentage[valley_class], 5)}% and {round(self.changing_percentage[valley_class + 1], 5)}%.')
-        print(min_output)
-        print(f'Price will probably change {round(self.changing_percentage[predicted_labels[-1]], 5)}% to {round(self.changing_percentage[predicted_labels[-1] + 1], 5)}% in 30 minutes from now.')
-        dataframe = self.total_data.iloc[int(np.ceil(self.learning_ratio * len(self.total_data))):]
-        print(dataframe['Close'].iloc[data_index:data_index + 30].values)
-        plt.plot(dataframe['Close'].iloc[data_index:data_index + 30].values)
-        plt.show()
+                max_output.append(i)
+        max_price = []
+        max_idx = []
+        for idx in max_output:
+            max_price.append(dataset['Close'].iloc[idx])
+            max_idx.append(idx)
 
-    def run(self):
-        self.train_loss, self.train_accuracy = self.model.evaluate(self.x_train, self.y_train, verbose=0)
-        self.test_loss, self.test_accuracy = self.model.evaluate(self.x_test, self.y_test, verbose=0)
-        self.train_class_accuracy = self.calculate_accuracy_for_each_class(self.x_train, self.y_train)
-        self.test_class_accuracy = self.calculate_accuracy_for_each_class(self.x_test, self.y_test)
+        maximum_upcoming_min = max_output[np.argmax(max_price)]
+        actual_change = round(((future_price / current_price) - 1) * 100, 5)
+        actual_class = y[data_index]
+        plot_data = dataset['Close'].iloc[data_index:data_index + 30].values
+        self.show_info(maximum_upcoming_min, predicted_labels[-1], actual_change, actual_class, plot_data, peak_class)
 
+    def run(self, x_train, y_train, x_test, y_test, testing_dataset, scale_data):
+        self.train_loss, self.train_accuracy = self.model.evaluate(x_train, y_train, verbose=0)
+        self.test_loss, self.test_accuracy = self.model.evaluate(x_test, y_test, verbose=0)
+        self.train_class_accuracy = self.calculate_accuracy_for_each_class(x_train, y_train)
+        self.test_class_accuracy = self.calculate_accuracy_for_each_class(x_test, y_test)
         self.show_accuracy()
         self.show_class_accuracy()
-        self.show_info()
-        self.find_peak(1566)
+        self.show_changing_percentage()
+        for i in range(2500, 2530):
+            self.predict(testing_dataset, x_test, y_test, i)
 
